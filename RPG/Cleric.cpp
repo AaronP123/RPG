95b6e30189282813bd6283bab70337062fb28bb2
AaronP123 <55748659+AaronP123@users.noreply.github.com>
2019-11-21 16:45:55 +0000

    Update Cleric.cpp

--- RPG/Cleric.cpp
+++ RPG/Cleric.cpp
@@ -1,29 +1,38 @@
 #include "Cleric.h"
 
 Cleric::Cleric() {
-	
+	pietyLevel_ = 0;
+
 }
 
-Cleric::Cleric(std::string name, float health, float weightLimit, Weapon weapon, Armour armour, int food, CharacterState state, int level) {
+Cleric::Cleric(std::string name, float health, float weightLimit, Weapon weapon, Armour armour, int food, CharacterState state, int level) :
+	GameCharacter( name, health, weightLimit, weapon, armour, food, state ), pietyLevel_{ level } {
 
 }
 
 void Cleric::SetPietyLevel(int level) {
-	
+	pietyLevel_ = level;
 }
 
 int Cleric::GetPietyLevel() const {
-	
+	return pietyLevel_;
 }
 
 bool Cleric::Attack(GameCharacter& character) {
-	
+	if (GameCharacter::Attack(character) ){
+		return true;
+	}
+	return false;
 }
 
 void Cleric::PrayFor(GameCharacter& character) {
-	
+	std::cout << GetCharacterName() << " is praying for " << character.GetCharacterName() << std::endl;
 }
 
+
 void Cleric::Sleep() {
-	
+	float tmp{ GetCharacterHealth() };
+	tmp = tmp + (tmp * 0.15);
+	SetCharacterHealth(tmp);
+	GameCharacter::Sleep();
 }
